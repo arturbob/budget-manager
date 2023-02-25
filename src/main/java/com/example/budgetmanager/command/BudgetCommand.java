496f100c833c7340446fc5d75e123a8f03057475
adamchuck <biznespat@gmail.com>
2023-02-25 22:42:04 +0100

    Budget validation added

--- src/main/java/com/example/budgetmanager/command/BudgetCommand.java
+++ src/main/java/com/example/budgetmanager/command/BudgetCommand.java
@@ -2,6 +2,7 @@ package com.example.budgetmanager.command;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @AllArgsConstructor
@@ -10,7 +11,10 @@ import java.time.LocalDate;
 @Setter
 @ToString
 public class BudgetCommand {
+    @NotBlank(message = "Your feature name of budget cannot be blank!")
     private String name;
+    @NotBlank(message = "Date of expiration cannot be blank, use patter YYYY-MM-DD")
     private LocalDate expirationDate;
+    @NotBlank(message = "Define your budget size!")
     private Long budgetSize;
 }

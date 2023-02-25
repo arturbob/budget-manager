a7f6fb1dd3fca29547af5f82707b67d1cbf7ec4b
adamchuck <biznespat@gmail.com>
2023-02-25 23:07:12 +0100

    Expense creation improved

--- src/main/java/com/example/budgetmanager/repository/BudgetRepository.java
+++ src/main/java/com/example/budgetmanager/repository/BudgetRepository.java
@@ -3,5 +3,8 @@ package com.example.budgetmanager.repository;
 import com.example.budgetmanager.domain.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
+    Optional<Budget> findBudgetByNameAndCustomer_Login(String budgetName, String login);
 }

b68ca72d85b1a491589ea070e7d20b1246a56ae1
adamchuck <biznespat@gmail.com>
2023-02-25 23:38:07 +0100

    creating customer (encoder, exc) improved

--- src/main/java/com/example/budgetmanager/service/CustomerService.java
+++ src/main/java/com/example/budgetmanager/service/CustomerService.java
@@ -1,15 +1,23 @@
 package com.example.budgetmanager.service;
 
 import com.example.budgetmanager.domain.Customer;
+import com.example.budgetmanager.exception.CustomerAlreadyExistException;
 import com.example.budgetmanager.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
-    public Customer save(Customer customer){
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public Customer save(Customer customer) {
+        if (customerRepository.existsCustomerByLogin(customer.getLogin())) {
+            throw new CustomerAlreadyExistException();
+        }
+        customer.setPassword(passwordEncoder.encode(customer.getPassword()));
         return customerRepository.save(customer);
     }
 }

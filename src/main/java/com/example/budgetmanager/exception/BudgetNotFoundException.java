f98b1432e759d1c20c85c341d52fabfc1b6cb6de
adamczak <biznespat@gmail.com>
2023-02-27 23:30:03 +0100

    Fixed response status

--- src/main/java/com/example/budgetmanager/exception/BudgetNotFoundException.java
+++ src/main/java/com/example/budgetmanager/exception/BudgetNotFoundException.java
@@ -3,9 +3,9 @@ package com.example.budgetmanager.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class BudgetNotFoundException extends RuntimeException{
-    public String getMessage(){
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class BudgetNotFoundException extends RuntimeException {
+    public String getMessage() {
         return "Budget with this name does not exist!";
     }
 }

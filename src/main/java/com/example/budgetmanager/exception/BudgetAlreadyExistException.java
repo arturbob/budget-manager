dc52ef91c78ee0a4cf5948d97c4987fdd39739ac
adamczak <biznespat@gmail.com>
2023-02-27 20:25:16 +0100

    add Response status

--- src/main/java/com/example/budgetmanager/exception/BudgetAlreadyExistException.java
+++ src/main/java/com/example/budgetmanager/exception/BudgetAlreadyExistException.java
@@ -1,5 +1,9 @@
 package com.example.budgetmanager.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BudgetAlreadyExistException extends RuntimeException {
     public String getMessage(){
         return "You have already created budget with this name";

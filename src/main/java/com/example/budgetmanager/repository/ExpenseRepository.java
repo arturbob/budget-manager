78519fccf5b7a9fe5eb04dfe13a9a2920cb100a1
adamczak <biznespat@gmail.com>
2023-02-28 20:53:24 +0100

    change method findAllByBudget_Name to findAllByBudget

--- src/main/java/com/example/budgetmanager/repository/ExpenseRepository.java
+++ src/main/java/com/example/budgetmanager/repository/ExpenseRepository.java
@@ -1,10 +1,11 @@
 package com.example.budgetmanager.repository;
 
+import com.example.budgetmanager.domain.Budget;
 import com.example.budgetmanager.domain.Expense;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-    Page<Expense> findAllByBudget_Name(String name, Pageable pageable);
+    Page<Expense> findAllByBudget(Budget budget, Pageable pageable);
 }

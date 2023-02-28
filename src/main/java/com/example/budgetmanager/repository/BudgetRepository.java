c4b7cc67c24408b1975dca13fe3bf8733a541552
adamczak <biznespat@gmail.com>
2023-02-28 20:51:51 +0100

    Add fetch query

--- src/main/java/com/example/budgetmanager/repository/BudgetRepository.java
+++ src/main/java/com/example/budgetmanager/repository/BudgetRepository.java
@@ -4,11 +4,13 @@ import com.example.budgetmanager.domain.Budget;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
-    Optional<Budget> findBudgetByNameAndCustomer_Login(String budgetName, String login);
+    @Query("SELECT b FROM Budget b LEFT JOIN FETCH b.expenses WHERE b.name = :budgetName AND b.customer.login = :login")
+    Optional<Budget> findBudgetByNameAndCustomerLogin(String budgetName, String login);
     boolean existsBudgetByNameAndCustomer_Login(String name, String login);
     Page<Budget> findBudgetsByCustomer_Login(String login, Pageable pageable);
 }

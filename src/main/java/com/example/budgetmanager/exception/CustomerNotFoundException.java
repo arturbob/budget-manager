32b8847f176d28cc14311d077632ef4bfeb3f3fc
adamczak <biznespat@gmail.com>
2023-03-06 21:00:05 +0100

    refactor and clean code

--- src/main/java/com/example/budgetmanager/exception/CustomerNotFoundException.java
+++ src/main/java/com/example/budgetmanager/exception/CustomerNotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class CustomerNotFoundException extends RuntimeException{
-    public String getMessage(){
+public class CustomerNotFoundException extends RuntimeException {
+    public String getMessage() {
         return "Customer with this login could not be found in database";
     }
 }
